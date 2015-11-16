052db03127a926d3c854e3a69c08341a40dfb834
David King <davidgwking@gmail.com>
2015-11-15 22:11:04 -0600

    style cleanup

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -2,6 +2,7 @@
 
 import config from '../config';
 import gulp   from 'gulp';
+
 import del    from 'del';
 
 gulp.task('clean', cb => del([config.buildDir], cb));
--- gulp/tasks/dev.js
+++ gulp/tasks/dev.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import gulp        from 'gulp';
+
 import runSequence from 'run-sequence';
 
 gulp.task('dev', cb => {
